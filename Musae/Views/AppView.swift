99ffec2e2dd2494d6bd91c99cab1cbdfc8041612
OberstKrueger <ok@oberstkrueger.com>
2020-09-25 15:30:35 -0700

    Changed to display MainView instead of dummy text

--- Musae/Views/AppView.swift
+++ Musae/Views/AppView.swift
@@ -5,7 +5,7 @@ struct AppView: View {
 
     var body: some View {
         if authorizer.status == .authorized {
-            Text("Authorized!")
+            MainView()
         } else {
             AuthorizationView(authorizer: authorizer)
         }
