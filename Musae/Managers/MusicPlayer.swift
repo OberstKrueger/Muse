06c0807ff457a42054e99e1633fe256fcf949833
OberstKrueger <ok@oberstkrueger.com>
2020-09-26 10:41:43 -0700

    Allowed nil MusicLibraryPlaylist to simplify unwrapping within UI code

--- Musae/Managers/MusicPlayer.swift
+++ Musae/Managers/MusicPlayer.swift
@@ -7,21 +7,23 @@ class MusicPlayer {
     let system = MPMusicPlayerController.systemMusicPlayer
 
     /// Plays the provided playlist shuffled, with least played songs played first.
-    func play(playlist: MusicLibraryPlaylist) {
-        DispatchQueue.global().async { [self] in
-            var songs: [MPMediaItem] = []
+    func play(playlist: MusicLibraryPlaylist?) {
+        if let existingPlaylist = playlist {
+            DispatchQueue.global().async { [self] in
+                var songs: [MPMediaItem] = []
 
-            for (_, value) in playlist.songs.sorted(by: {$0.key < $1.key}) {
-                songs.append(contentsOf: value.shuffled())
-            }
+                for (_, value) in existingPlaylist.songs.sorted(by: {$0.key < $1.key}) {
+                    songs.append(contentsOf: value.shuffled())
+                }
 
-            let collection = MPMediaItemCollection(items: songs)
+                let collection = MPMediaItemCollection(items: songs)
 
-            DispatchQueue.main.async {
-                system.setQueue(with: collection)
-                system.prepareToPlay()
-                system.play()
-            }
+                DispatchQueue.main.async {
+                    system.setQueue(with: collection)
+                    system.prepareToPlay()
+                    system.play()
+                }
+        }
         }
     }
 }
