55774556d1af98788d02cc2017e34302dca1c02e
OberstKrueger <ok@oberstkrueger.com>
2020-09-26 10:33:54 -0700

    Created music player

---
+++ Musae/Managers/MusicPlayer.swift
@@ -0,0 +1,27 @@
+import Foundation
+import MediaPlayer
+
+/// Plays provided playlists and songs through the system music player.
+class MusicPlayer {
+    /// System music player
+    let system = MPMusicPlayerController.systemMusicPlayer
+
+    /// Plays the provided playlist shuffled, with least played songs played first.
+    func play(playlist: MusicLibraryPlaylist) {
+        DispatchQueue.global().async { [self] in
+            var songs: [MPMediaItem] = []
+
+            for (_, value) in playlist.songs.sorted(by: {$0.key < $1.key}) {
+                songs.append(contentsOf: value.shuffled())
+            }
+
+            let collection = MPMediaItemCollection(items: songs)
+
+            DispatchQueue.main.async {
+                system.setQueue(with: collection)
+                system.prepareToPlay()
+                system.play()
+            }
+        }
+    }
+}
