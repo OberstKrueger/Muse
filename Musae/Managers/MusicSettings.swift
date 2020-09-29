62388ac81b131e8c660eba2bad5ecdf8d047296e
OberstKrueger <ok@oberstkrueger.com>
2020-09-29 04:20:44 -0700

    Added setting to sort by average play count

--- Musae/Managers/MusicSettings.swift
+++ Musae/Managers/MusicSettings.swift
@@ -2,6 +2,8 @@ import Combine
 
 /// User settings.
 class MusicSettings: ObservableObject {
+    /// Sort playlist view by average playcount instead of alphabetically.
+    @Published var sortByAveragePlayCount: Bool = true
     /// Number of minutes for Up Next play.
     @Published var upNextMinutes: UInt = 32
 }
