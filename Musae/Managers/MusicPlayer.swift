0802312751c111989c9ffbd19424c4c9301d005f
OberstKrueger <ok@oberstkrueger.com>
2021-03-20 15:32:41 -0700

    Changed Up Next to select half lowest played items and half random items

--- Musae/Managers/MusicPlayer.swift
+++ Musae/Managers/MusicPlayer.swift
@@ -35,28 +35,33 @@ class MusicPlayer {
     /// Adds least played items from provided playlist to the Up Next queue.
     func upNext(playlist: MusicPlaylist?, category: String, minutes: Int) {
         logger.info("Adding to up next: \(category) - \(playlist?.title ?? "no playlist provided")")
-        if let existingPlaylist = playlist {
+        if let existingPlaylist = playlist?.songs.sorted(by: {$0.key < $1.key}).flatMap({$0.value.shuffled()}) {
             DispatchQueue.global().async { [self] in
+                var duration: Float64 = 0
                 var songs: [MPMediaItem] = []
-                var totalDuration: Float64 = 0
+                var targetLength: Float64 = ((Float64(minutes) / 2) * 60) // Cut in half for two-part process.
 
-                for (_, value) in existingPlaylist.songs.sorted(by: {$0.key < $1.key}) {
-                    var upcomingSongs = value.shuffled()
-
-                    while totalDuration < Float64(minutes * 60) && upcomingSongs.isEmpty == false {
-                        if let song = upcomingSongs.popLast() {
-                            songs.append(song)
-                            totalDuration += song.playbackDuration
-                        }
-                    }
-
-                    if totalDuration > Float64(minutes * 60) {
+                for song in existingPlaylist {
+                    if duration < targetLength {
+                        duration += song.playbackDuration
+                        songs.append(song)
+                    } else {
                         break
                     }
+                }
+                // Adjust to full-length of minutes.
+                targetLength *= 2
+                while duration < targetLength {
+                    let random = existingPlaylist.randomElement()!
+
+                    duration += random.playbackDuration
+                    songs.append(random)
                 }
 
                 let collection = MPMediaItemCollection(items: songs)
                 let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: collection)
+
+                logger.info("Adding \(songs.count) songs to the Up Next queue.")
 
                 DispatchQueue.main.async {
                     system.prepend(descriptor)
