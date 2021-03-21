ef001a7839d383a729727a0cd34d6bcda5a424a9
OberstKrueger <ok@oberstkrueger.com>
2021-03-21 12:03:22 -0700

    Corrected logic for up next selections

--- Musae/Managers/MusicPlayer.swift
+++ Musae/Managers/MusicPlayer.swift
@@ -37,31 +37,54 @@ class MusicPlayer {
         logger.info("Adding to up next: \(category) - \(playlist?.title ?? "no playlist provided")")
         if let existingPlaylist = playlist?.songs.sorted(by: {$0.key < $1.key}).flatMap({$0.value.shuffled()}) {
             DispatchQueue.global().async { [self] in
+                let playlistTotal = existingPlaylist.reduce(0, {$0 + $1.playbackDuration})
+
                 var duration: Float64 = 0
                 var songs: [MPMediaItem] = []
-                var targetLength: Float64 = ((Float64(minutes) / 2) * 60) // Cut in half for two-part process.
+                var targetLength: Float64 = (Float64(minutes) * 60) / 2     // Cut in half for two-part process.
 
-                for song in existingPlaylist {
-                    if duration < targetLength {
+                if playlistTotal > Float64(minutes * 60) {
+                    logger.info("Playlist length exceeds user settings value.")
+
+                    for song in existingPlaylist {
                         duration += song.playbackDuration
                         songs.append(song)
-                    } else {
-                        break
-                    }
-                }
-                // Adjust to full-length of minutes.
-                targetLength *= 2
-                while duration < targetLength {
-                    let random = existingPlaylist.randomElement()!
 
-                    duration += random.playbackDuration
-                    songs.append(random)
+                        if duration > targetLength { break }
+                    }
+
+                    targetLength *= 2
+
+                    while duration < targetLength {
+                        let randomSong = existingPlaylist.randomElement()!
+
+                        if songs.contains(randomSong) == false {
+                            duration += randomSong.playbackDuration
+                            songs.append(randomSong)
+                        }
+                    }
+
+                    songs.shuffle()
+                } else {
+                    logger.info("Playlist length below mininum user settings value.")
+
+                    duration = playlistTotal
+                    songs = existingPlaylist.shuffled()
+                    targetLength *= 2   // Two-part process unnecessary
+
+                    while duration < targetLength {
+                        let randomSong = existingPlaylist.randomElement()!
+
+                        duration += randomSong.playbackDuration
+                        songs.append(randomSong)
+                    }
                 }
 
                 let collection = MPMediaItemCollection(items: songs)
                 let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: collection)
 
                 logger.info("Adding \(songs.count) songs to the Up Next queue.")
+                logger.info("Total song length added: \(songs.reduce(0, {$0 + $1.playbackDuration})) seconds.")
 
                 DispatchQueue.main.async {
                     system.prepend(descriptor)
