cefb52352a1d5e23dba60330ba9a83037394f2ec
OberstKrueger <ok@oberstkrueger.com>
2021-06-03 20:19:13 -0700

    Made logger fileprivate

--- Musae/Managers/LibraryManager.swift
+++ Musae/Managers/LibraryManager.swift
@@ -1,1 +1,7 @@
-class LibraryManager {}
+import os
+
+class LibraryManager {
+    // MARK: - Internal Properties
+    /// System logger.
+    fileprivate let logger = Logger(subsystem: "technology.krueger.musae", category: "library")
+}
