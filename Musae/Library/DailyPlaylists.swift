33733437ed3d318fd234b22065c47f1a86af1c97
OberstKrueger <ok@oberstkrueger.com>
2021-06-05 18:55:22 -0700

    Updated algorithm

--- Musae/Library/DailyPlaylists.swift
+++ Musae/Library/DailyPlaylists.swift
@@ -13,27 +13,26 @@ struct DailyPlaylists {
     init(_ libraryPlaylists: [String: [MusicPlaylist]]) {
         let defaults = Defaults()
 
-        for key in libraryPlaylists.keys {
-            /// Results with an average playcount above or equal to 1.
-            let resultsAll: [String] = libraryPlaylists[key, default: []]
-                .filter({$0.averagePlayCount.isNaN == false})
-                .sorted(by: {$0.averagePlayCount < $1.averagePlayCount})
-                .map({($0.title)})
-            /// Results with an average playcount below 1.
-            let resultsBelow: [String] = libraryPlaylists[key, default: []]
-                .filter({$0.averagePlayCount < 1})
-                .map({$0.title})
+        for (key, value) in libraryPlaylists {
+            var selection: Int {
+                let check: Int = value.count / 2
 
-            if let playlist = resultsBelow.randomElement() {
-                playlists[key] = playlist
-            } else {
-                switch resultsAll.count {
-                case ...0: playlists[key] = "No valid playlists"
-                case ...4: playlists[key] = resultsAll[0]
-                case ...8: playlists[key] = resultsAll[...3].randomElement()!
-                default:   playlists[key] = resultsAll[...7].randomElement()!
+                switch check {
+                case ...0: return 0
+                default:
+                    var result: Int = 1
+                    for _ in 1..<(64 - check.leadingZeroBitCount) {
+                        result *= 2
+                    }
+                    return result
                 }
             }
+
+            let sorted: [String] = value.filter({$0.averagePlayCount.isNaN == false})
+                .sorted(by: {$0.averagePlayCount < $1.averagePlayCount})
+                .map({$0.title})
+
+            playlists[key] = sorted[...selection].randomElement()!
         }
 
         date = Date()
