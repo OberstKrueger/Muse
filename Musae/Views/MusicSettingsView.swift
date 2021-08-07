4075726705bacad3b41c88ac1fd255cbef699f2e
OberstKrueger <ok@oberstkrueger.com>
2021-08-07 04:36:43 -0700

    Added display of playlists too small for current up next minutes

--- Musae/Views/MusicSettingsView.swift
+++ Musae/Views/MusicSettingsView.swift
@@ -4,12 +4,35 @@ struct MusicSettingsView: View {
     @EnvironmentObject var settings: MusicSettings
     @ObservedObject var library: LibraryManager
 
+    var smallPlaylists: [String] {
+        get {
+            var results: [String] = []
+
+            for (category, playlists) in library.categories {
+                for playlist in playlists {
+                    if Int(playlist.length) < settings.upNextMinutes {
+                        results.append("\(category) - \(playlist.title)")
+                    }
+                }
+            }
+
+            return results.sorted()
+        }
+    }
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     Toggle("Sort By Average Play Count", isOn: $settings.sortByAveragePlayCount)
                     Stepper("Up Next Minutes: \(settings.upNextMinutes)", value: $settings.upNextMinutes)
+                }
+                if smallPlaylists.count > 0 {
+                    Section(header: Text("Small Playlists")) {
+                        ForEach(smallPlaylists, id: \.self) { playlist in
+                            Text(playlist)
+                        }
+                    }
                 }
                 if let date = library.lastUpdated {
                     Text("Library Updated: \(date)")
