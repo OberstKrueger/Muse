cae45fbdd1672a640d67dd05f603be6d8587225b
OberstKrueger <ok@oberstkrueger.com>
2021-08-28 15:35:33 -0700

    Added retrieval of playlist songs.

--- Musae/Library/Playlist.swift
+++ Musae/Library/Playlist.swift
@@ -4,10 +4,7 @@ import MediaPlayer
 struct Playlist {
     // MARK: - Initializations
     init(_ playlist: MPMediaPlaylist, _ title: String) {
-        // Set id.
-        id = playlist.persistentID
-
-        // Set averagePlayCount.
+        // Set averagePlayCount and length.
         if playlist.items.count == 0 {
             averagePlayCount = .nan
             length = .nan
@@ -15,6 +12,9 @@ struct Playlist {
             averagePlayCount = Float64(playlist.items.reduce(0, {$0 + $1.playCount})) / Float64(playlist.items.count)
             length = playlist.items.reduce(0, {$0 + $1.playbackDuration}) / 60
         }
+
+        // Set id.
+        id = playlist.persistentID
 
         // Set title.
         self.title = title
@@ -29,6 +29,17 @@ struct Playlist {
 
     /// Length in minutes of the playlist.
     let length: Float64
+
+    /// Songs that make up the playlist.
+    var songs: [MPMediaItem] {
+        let predicate = MPMediaPropertyPredicate(value: id,
+                                                 forProperty: MPMediaPlaylistPropertyPersistentID, comparisonType: .equalTo)
+        let query = MPMediaQuery()
+
+        query.addFilterPredicate(predicate)
+
+        return query.items ?? []
+    }
 
     /// Title of the playlist.
     let title: String
