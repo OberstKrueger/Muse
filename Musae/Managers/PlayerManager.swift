bb4b763ddf5181291ddf8a839a39b8424457b7f3
OberstKrueger <ok@oberstkrueger.com>
2021-08-28 16:25:05 -0700

    Changed to use internal playlist query

--- Musae/Managers/PlayerManager.swift
+++ Musae/Managers/PlayerManager.swift
@@ -31,83 +31,80 @@ class PlayerManager {
     func play(_ playlist: Playlist) {
         logger.info("Starting playlist: \(playlist.title)")
 
-        if let query = query(playlist.id) {
-            let items = Dictionary(grouping: query.items, by: {$0.playCount})
-            var songs: [MPMediaItem] = []
+        let songs = Dictionary(grouping: playlist.songs, by: {$0.playCount})
+        var toPlay: [MPMediaItem] = []
 
-            for (_, value) in items.sorted(by: {$0.key < $1.key}) {
-                songs.append(contentsOf: value.shuffled())
-            }
-            let collection = MPMediaItemCollection(items: songs)
-
-            system.setQueue(with: collection)
-            system.prepareToPlay()
-            system.play()
+        for (_, value) in songs.sorted(by: {$0.key < $1.key}) {
+            toPlay.append(contentsOf: value.shuffled())
         }
+
+        let collection = MPMediaItemCollection(items: toPlay)
+
+        system.setQueue(with: collection)
+        system.prepareToPlay()
+        system.play()
     }
 
     /// Adds assortment of items to the Up Next queue, favoring lesser played items.
     func upNext(_ playlist: Playlist, _ minutes: Int) {
         logger.info("Adding to up next: \(playlist.title)")
 
-        if let query = query(playlist.id) {
-            let items = Dictionary(grouping: query.items, by: {$0.playCount})
-            let playlistTotal: Float64 = query.items.reduce(0, {$0 + $1.playbackDuration})
+        let items = Dictionary(grouping: playlist.songs, by: {$0.playCount})
+        let playlistTotal: Float64 = playlist.songs.reduce(0, {$0 + $1.playbackDuration})
 
-            var duration: Float64 = 0
-            var songs: [MPMediaItem] = []
-            var target: Float64 = (Float64(minutes) * 60) / 2   // Cut in half for two-part response.
+        var duration: Float64 = 0
+        var songs: [MPMediaItem] = []
+        var target: Float64 = (Float64(minutes) * 60) / 2   // Cut in half for two-part response.
 
-            if playlistTotal > (target * 2) {
-                logger.info("Playlist length is longer than user settings value.")
+        if playlistTotal > (target * 2) {
+            logger.info("Playlist length is longer than user settings value.")
 
-                firstPass: for (_, value) in items.sorted(by: {$0.key < $1.key}) {
-                    for song in value.shuffled() {
-                        logger.info("Adding song to Up Next Queue: \(song.title!), \(song.playCount) plays.")
+            firstPass: for (_, value) in items.sorted(by: {$0.key < $1.key}) {
+                for song in value.shuffled() {
+                    logger.info("Adding song to Up Next Queue: \(song.title!), \(song.playCount) plays.")
 
-                        duration += song.playbackDuration
-                        songs.append(song)
+                    duration += song.playbackDuration
+                    songs.append(song)
 
-                        if duration > target { break firstPass }
-                    }
-                }
-
-                target *= 2
-
-                while duration < target {
-                    let song = query.items.randomElement()!
-
-                    if songs.contains(song) == false {
-                        logger.info("Adding song to Up Next Queue: \(song.title!), \(song.playCount) plays.")
-
-                        duration += song.playbackDuration
-                        songs.append(song)
-                    }
-                }
-
-                songs.shuffle()
-            } else {
-                logger.info("Playlist length is shorter than user settings value.")
-
-                duration = playlistTotal
-                songs = query.items.shuffled()
-                target *= 2
-
-                while duration < target {
-                    let random = query.items.randomElement()!
-
-                    duration += random.playbackDuration
-                    songs.append(random)
+                    if duration > target { break firstPass }
                 }
             }
 
-            let collection = MPMediaItemCollection(items: songs)
-            let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: collection)
+            target *= 2
 
-            logger.info("Adding \(songs.count) songs to the Up Next queue.")
-            logger.info("Total song length added: \(songs.reduce(0, {$0 + $1.playbackDuration})) seconds.")
+            while duration < target {
+                let song = playlist.songs.randomElement()!
 
-            system.prepend(descriptor)
+                if songs.contains(song) == false {
+                    logger.info("Adding song to Up Next Queue: \(song.title!), \(song.playCount) plays.")
+
+                    duration += song.playbackDuration
+                    songs.append(song)
+                }
+            }
+
+            songs.shuffle()
+        } else {
+            logger.info("Playlist length is shorter than user settings value.")
+
+            duration = playlistTotal
+            songs = playlist.songs.shuffled()
+            target *= 2
+
+            while duration < target {
+                let random = playlist.songs.randomElement()!
+
+                duration += random.playbackDuration
+                songs.append(random)
+            }
         }
+
+        let collection = MPMediaItemCollection(items: songs)
+        let descriptor = MPMusicPlayerMediaItemQueueDescriptor(itemCollection: collection)
+
+        logger.info("Adding \(songs.count) songs to the Up Next queue.")
+        logger.info("Total song length added: \(songs.reduce(0, {$0 + $1.playbackDuration})) seconds.")
+
+        system.prepend(descriptor)
     }
 }
